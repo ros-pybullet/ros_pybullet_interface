7aeff83bf6ca0dda04fa5d4668c37e7c4bfa4796
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-12-24 09:53:44 +0000

    Update rpbi_node.py implementation

--- ros_pybullet_interface/ros_pybullet_interface/rpbi_node.py
+++ ros_pybullet_interface/ros_pybullet_interface/rpbi_node.py
@@ -1,29 +1,35 @@
-import rclpy
-from rclpy.node import Node
+from .node import RPBIBaseNode, main_func
+
 from std_msgs.msg import Bool
-
-import pybullet as p
-
-default_sampling_frequency = 20
 
 class ActiveStatusPublisher:
 
+    """Publishes whether the interface is active or not on the topic rpbi/active."""
+
     def __init__(self, node):
-        node.declare_parameter('active_publish_frequency', default_sampling_frequency)
-        active_publish_frequency = int(node.get_parameter(
-            'active_publish_frequency').get_parameter_value().integer_value
-        )
-        self.pub = node.create_publisher(Bool, 'rpbi/active', qos_profile=10)
-        node.create_timer(1./float(active_publish_frequency), self.active_status_timer_callback)
+        """Class constructor."""
+
+        # Class attributes
         self.node = node
 
+        # Setup publisher
+        self.pub = self.node.create_publisher(Bool, 'rpbi/active', qos_profile=10)
+
+        # Start timer
+        dt = 1./float(self.node.active_publish_frequency)
+        self.node.create_timer(dt, self.active_status_timer_callback)
+
     def active_status_timer_callback(self):
+        """Status publisher timer callback."""
         self.pub.publish(Bool(data=self.node.active))
 
 
-class ROSPyBulletInterfaceNode(Node):
+class ROSPyBulletInterfaceNode(RPBIBaseNode):
+
+    """The main ROS-PyBullet Interface server node."""
 
     def __init__(self):
+        """Node constructor"""
 
         # Initialize ROS
         super().__init__('rpbi_node', server_node=True)
@@ -31,31 +37,43 @@ class ROSPyBulletInterfaceNode(Node):
         # Setup class attributes
         self.active = False
 
-        # Connect to PyBullet
-        connect_config = {}
-
-        self.declare_parameter('connect_options', '')
-        connect_options = self.get_parameter(
-            'connect_options',
-        ).get_parameter_value().string_value
-        if connect_options:
-            connect_config['options'] = connect_options
-
-        self.client_id = p.connect(p.GUI_SERVER, **connect_config)
-
         # Start active status publisher
         ActiveStatusPublisher(self)
 
-        p.setTimeStep(0.01)
-        p.setGravity(0, 0, -10)
+        # Setup PyBullet
+        set_time_step = self.setTimeStep
+        if set_time_step is not None:
+            p.setTimeStep(**set_time_step)
+
+        set_gravity = self.setGravity
+        if set_gravity is not None:
+            p.setGravity(**set_gravity)
+
+        # TEMP
         p.setRealTimeSimulation(1)
+        # TEMP
+
         self.active = True
 
 
-def main():
-    rclpy.init()
-    node = ROSPyBulletInterfaceNode()
-    rclpy.spin(node)
+    # ----------------------------------
+    # Properties/configuration
 
-if __name__ == '__main__':
-    main()
+    @property
+    def active_publish_frequency(self):
+        """Sampling frequency that the active status is published."""
+        return self.node.get('active_publish_frequency', 20)
+
+    @property
+    def setTimeStep(self):
+        """https://docs.google.com/document/d/10sXEhzFRSnvFcl3XxNGhnD4N2SedqwdAvK3dsihxVUA/edit#heading=h.5h9ku1yf3u1v"""
+        return self.config.get('setTimeStep')
+
+    @property
+    def setGravity(self):
+        """https://docs.google.com/document/d/10sXEhzFRSnvFcl3XxNGhnD4N2SedqwdAvK3dsihxVUA/edit#heading=h.d6ihmmtes1id"""
+        return self.config.get('setGravity')
+
+
+def main():
+    main_func(ROSPyBulletInterfaceNode)
